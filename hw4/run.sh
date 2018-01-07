9b3cee4eda5329cb061766132dae2c73f0f515cf
tychen5 <32350278+tychen5@users.noreply.github.com>
2018-01-08 00:02:43 +0800

    UPDATE
    UPDATE

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+TEST_TEXT=$1
 wget https://www.dropbox.com/s/z51aiw95xn6oz7i/-61.data-00000-of-00001?dl=0 -O ./models/-61.data-00000-of-00001
-python3.5 train_generate.py $1
+python3.5 train_generate.py --test_text $TEST_TEXT
