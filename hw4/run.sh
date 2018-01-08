c58c7c7bc8f662092ab9f6166dfe89e0497064fc
tychen5 <32350278+tychen5@users.noreply.github.com>
2018-01-08 08:35:07 +0800

    上傳正確的script
    原先script不小心上傳到舊版

--- hw4/run.sh
+++ hw4/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget https://www.dropbox.com/s/z51aiw95xn6oz7i/-61.data-00000-of-00001?dl=0 -O ./models/-61.data-00000-of-00001
-python3.5 train_generate.py 
+python3.5 train_generate.py $1
